426d18059a4eb709eae5700a6df29ed8e1e8c56d
Escarter <mbutuhescarter@gmail.com>
2023-08-13 10:44:55 +0100

    updates on sales

--- resources/views/livewire/portal/lotissements/sale.blade.php
+++ resources/views/livewire/portal/lotissements/sale.blade.php
@@ -39,27 +39,46 @@
         </div>
     </div>
     <div class='row'>
-        <div class='col-md-2 '>
+        <div class='col-md-3 '>
             <div class="card p-3 mb-3">
                 <h5 class="w-auto">{{__('Simple Sale')}}</h5>
                 <ul>
                     <li>{{__('Update Land information')}}</li>
                     <li>{{__('Update Promoter information')}}</li>
                 </ul>
+            </div>
+            <div class="card p-3 mb-3">
+                <h5 class="w-auto">{{__('Contacts')}}</h5>
             </div>
             <div class="card p-3">
-                <h5 class="w-auto">{{__('Simple Sale')}}</h5>
-                <ul>
-                    <li>{{__('Update Land information')}}</li>
-                    <li>{{__('Update Promoter information')}}</li>
-                </ul>
+                <h5 class="w-auto">{{__('Basic Stats')}}</h5>
+
+                <div class="d-flex align-items-center mb-2 border-1 border-bottom">
+                    <span class="fw-light ">{{__('Total Blocks')}}</span> : <span class="fw-bolder mx-2">{{count($lotissement->blocks)}} </span>
+                </div>
+                <div class="d-flex align-items-center mb-2 border-1 border-bottom">
+                    <span class="fw-light ">{{__('Total Lots')}}</span> : <span class="fw-bolder mx-2">{{count($lotissement->parcels)}} </span>
+                </div>
+                <div class="d-flex align-items-center mb-2 border-1 border-bottom">
+                    <span class="fw-light ">{{__('Total Public Lots')}}</span> : <span class="fw-bolder mx-2">{{count($lotissement->parcels->where('type','public'))}} </span>
+                </div>
+                <div class="d-flex align-items-center mb-2 border-1 border-bottom">
+                    <span class="fw-light ">{{__('Total Normal Lots')}}</span> : <span class="fw-bolder mx-2">{{count($lotissement->parcels->where('type','normale'))}} </span>
+                </div>
+                <div class="d-flex align-items-center mb-2 border-1 border-bottom">
+                    <span class="fw-light ">{{__('Total Lots Sold')}}</span> : <span class="fw-bolder mx-2">{{count($lotissement->parcels->where('date_de_vente','<>',null))}} </span>
+                </div>
+                <div class="d-flex align-items-center mb-2">
+                    <span class="fw-light ">{{__('Total Lots Remaining')}}</span> : <span class="fw-bolder mx-2">{{count($lotissement->parcels->where('date_de_vente',null))}} </span>
+                </div>
             </div>
+
         </div>
-        <div class='col-md-10'>
-            <div class="card p-3 mb-3">
+        <div class='col-md-9'>
+            <div class="card p-4 mb-3">
                 <legend class="w-auto">Informations on the Land</legend>
-                <div class='d-flex justify-content-between me-2'>
-                    <div>
+                <div class='row gap-4'>
+                    <div class="col">
                         <a href="#" class="d-flex align-items-center justify-content-between">
                             <div class="d-flex ">
                                 <div class="avatar avatar-md d-flex align-items-center justify-content-center fw-bold rounded bg-primary me-3">
@@ -92,7 +111,29 @@
                                         <h6 class="mb-0" style="font-size: small;">{{number_format($lotissement->titreFoncier->superficie_du_TF_mere * 0.0001, 2)}} ha</h6>
                                     </div>
                                 </div>
-                                <div class="d-flex align-items-center ">
+                                <div class="d-flex align-items-center me-3 ">
+                                    <div class="icon-shape icon-xs icon-shape-purple rounded me-2">
+                                        <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z"></path>
+                                        </svg>
+                                    </div>
+                                    <div class="d-block">
+                                        <label class="mb-0" style="font-size: small;">Sessions</label>
+                                        <h6 class="mb-0" style="font-size: small;">9,567</h6>
+                                    </div>
+                                </div>
+                                <div class="d-flex align-items-center me-3">
+                                    <div class="icon-shape icon-xs icon-shape-purple rounded me-3">
+                                        <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z"></path>
+                                        </svg>
+                                    </div>
+                                    <div class="d-block">
+                                        <label class="mb-0" style="font-size: small;">Sessions</label>
+                                        <h6 class="mb-0" style="font-size: small;">9,567</h6>
+                                    </div>
+                                </div>
+                                <div class="d-flex align-items-center me-3">
                                     <div class="icon-shape icon-xs icon-shape-purple rounded me-2">
                                         <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z"></path>
@@ -104,9 +145,56 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class=''>
-                                {{__('Coordinates')}} : {{$lotissement->titreFoncier->coordonnees}}
+                            <span class="fw-bolder mt-2">{{__('Contacts')}}</span>
+                            <div class='d-flex justify-content-between border-bottom '>
+                                <a href="#" class="d-flex align-items-center  py-1">
+                                    <div class="avatar  d-flex align-items-center justify-content-center fw-bold  rounded bg-primary me-2"><span class="text-white" style="font-size: small;">{{initials($lotissement->maeture)}}</span></div>
+                                    <div class="d-block">
+                                        <span class="fw-bold " style="font-size: small;">{{ucwords($lotissement->maeture)}}</span>
+                                        <div class="small text-gray" style="font-size: x-small;">
+                                            <svg class="icon icon-xxs me-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"></path>
+                                            </svg> {{__('Maeture')}}
+                                        </div>
+                                    </div>
+                                </a>
+                                <a href="#" class="d-flex align-items-center py-1">
+                                    <div class="avatar  d-flex align-items-center justify-content-center fw-bold  rounded bg-primary me-2"><span class="text-white" style="font-size: small;">{{initials($lotissement->promoteur_immobiliere)}}</span></div>
+                                    <div class="d-block">
+                                        <span class="fw-bold " style="font-size: small;">{{ucwords($lotissement->promoteur_immobiliere)}}</span>
+                                        <div class="small text-gray" style="font-size: x-small;">
+                                            <svg class="icon icon-xxs me-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"></path>
+                                            </svg> {{__('Promoteur immobiliere')}}
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
+                            <div class='d-flex justify-content-between border-bottom '>
+                                <a href="#" class="d-flex align-items-center  py-1">
+                                    <div class="avatar  d-flex align-items-center justify-content-center fw-bold  rounded bg-primary me-2"><span class="text-white" style="font-size: small;">{{initials($lotissement->agent_immobiliere)}}</span></div>
+                                    <div class="d-block">
+                                        <span class="fw-bold " style="font-size: small;">{{ucwords($lotissement->agent_immobiliere)}}</span>
+                                        <div class="small text-gray" style="font-size: x-small;">
+                                            <svg class="icon icon-xxs me-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"></path>
+                                            </svg> {{__('Agent immobiliere')}}
+                                        </div>
+                                    </div>
+                                </a>
+                                <a href="#" class="d-flex align-items-center py-1">
+                                    <div class="avatar  d-flex align-items-center justify-content-center fw-bold  rounded bg-primary me-2"><span class="text-white" style="font-size: small;">{{initials($lotissement->promoteur_immobiliere)}}</span></div>
+                                    <div class="d-block">
+                                        <span class="fw-bold " style="font-size: small;">{{ucwords($lotissement->promoteur_immobiliere)}}</span>
+                                        <div class="small text-gray" style="font-size: x-small;">
+                                            <svg class="icon icon-xxs me-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"></path>
+                                            </svg> {{__('Promoteur immobiliere')}}
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
 
                             <!-- {{$lotissement->titreFoncier->superficie_du_TF_mere}}
                             {{$lotissement->titreFoncier->superficie_vendue_du_TF_mere}}
@@ -114,13 +202,16 @@
 
                         </div>
                         @if(!empty($lotissement->titreFoncier))
-                        <div class="d-flex align-items-center">
-                            {{__('Location')}} : <span class="fw-bold mx-2"> {{$lotissement->titreFoncier->region->region_name}} > {{$lotissement->titreFoncier->division->division_name}} > {{$lotissement->titreFoncier->subDivision->sub_division_name}} > {{$lotissement->titreFoncier->lieu_dit}} </span>
+                        <div class="d-flex align-items-center mb-2">
+                            <span class="fw-bolder ">{{__('Location')}}</span> : <span class="fw-light mx-2"> {{$lotissement->titreFoncier->region->region_name}} > {{$lotissement->titreFoncier->division->division_name}} > {{$lotissement->titreFoncier->subDivision->sub_division_name}} > {{$lotissement->titreFoncier->lieu_dit}} </span>
+                        </div>
+                        <div class='mb-2'>
+                            <span class="fw-bolder mt-2">{{__('Coordinates')}}</span> : {{$lotissement->titreFoncier->coordonnees}}
                         </div>
                         @endif
                     </div>
-                    <div>
-                        <h4 class="fs-5">{{__('Owners')}}</h4>
+                    <div class="col">
+                        <h4 class="fs-5">{{__('Land Title Owners')}}</h4>
                         @if(!empty($lotissement->titreFoncier))
                         @foreach($lotissement->titreFoncier->users as $key=>$option)
                         <a href="#" class="d-flex align-items-center {{!$loop->last ? 'border-bottom' : ''}} py-1">
@@ -144,32 +235,45 @@
             </div>
             <div class="card p-4 ">
                 <legend class="w-auto">{{__('Blocks and Lots')}}</legend>
-
                 @foreach($lotissement->blocks as $block)
-                <div class='mb-3'>
-                    @if(!empty($block->parcels->where('date_de_vente', null)->where('type','normale')))
+                <div class='mb-3 px-3'>
+                    @if(!empty($block->parcels))
                     <span class="fw-bold pb-3"> {{__('Block Name')}} : {{$block->block_name}}</span>
-
-                    <div class='d-flex justify-content-between border border-1 fw-bold px-3 mt-1'>
-                        <span>{{__('Numero Lot')}}</span>
-                        <span>{{__('Superficie Total')}}</span>
-                        <span>{{__('Statut Lot')}}</span>
-                        <span>{{__('Action')}}</span>
+                    <div class='row border border-1 fw-bold  mt-1'>
+                        <span class="col py-1 text-left">{{__('Numero Lot')}}</span>
+                        <span class="col py-1 text-left">{{__('Superficie Total')}}</span>
+                        <span class="col py-1 text-left"> {{__('Statut Lot')}}</span>
+                        <span class="col py-1 text-left"> {{__('Type Lot')}}</span>
+                        <span class="col py-1 text-left"> {{__('Etat Vente')}}</span>
+                        <span class="col py-1 text-end">{{__('Action')}}</span>
                     </div>
                     @endif
-                    @foreach($block->parcels->where('date_de_vente', null)->where('type','normale') as $lot)
+                    @foreach($block->parcels as $lot)
                     @if(!empty($lot))
-                    <div class='d-flex justify-content-between align-items-baseline border border-1 px-3 '>
-                        <span class="py-1 text-center">{{$lot->numero_du_lot}}</span>
-                        <span class="py-1 text-center">{{$lot->surperficie_du_lot}}</span>
-                        <span class="py-1 text-center">{{$lot->statut_du_lot}}</span>
-                        <span class="py-1text-center">
+                    <div class='row border border-1 px-3 mt-1 '>
+                        <span class="col py-1 mr-auto text-right">{{$lot->numero_du_lot}}</span>
+                        <span class="col py-1 mr-auto text-right">{{$lot->surperficie_du_lot}}</span>
+                        <span class="col py-1 mr-auto text-right">{{$lot->statut_du_lot}}</span>
+                        <span class="col py-1 mr-auto text-right">{{$lot->type}}</span>
+                        <span class="col py-1 mr-auto text-right">
+                            @if(is_null($lot->date_de_vente) && $lot->type == 'normale')
+                            {{__('disponible')}}
+                            @endif
+                        </span>
+
+                        @if(is_null($lot->date_de_vente) && $lot->type == 'normale')
+                        <span class="col py-1  text-end">
                             <a href="">
                                 <svg class="icon icon-sm text-info" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
                                 </svg>
                             </a>
                         </span>
+                        @else
+                        <span class="col py-1  text-end">
+                            {{__('Non-disponible')}}
+                        </span>
+                        @endif
                     </div>
                     @endif
                     @endforeach
