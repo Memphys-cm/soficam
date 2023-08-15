c0ccf7c92e52b402e457d7390d7b6df941d4660c
Escarter <mbutuhescarter@gmail.com>
2023-08-15 15:51:52 +0100

    cleanup

--- resources/views/livewire/portal/maps/index.blade.php
+++ resources/views/livewire/portal/maps/index.blade.php
@@ -1,77 +1,66 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Affichage des Titres Fonciers</title>
-<meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
-<link href="https://api.mapbox.com/mapbox-gl-js/v2.15.0/mapbox-gl.css" rel="stylesheet">
-<script src="https://api.mapbox.com/mapbox-gl-js/v2.15.0/mapbox-gl.js"></script>
-<style>
-body { margin: 0; padding: 0; }
-#map { position: absolute; top: 0; bottom: 0; width: 100%; overflow:hidden }
-</style>
-</head>
-<body>
-<div id="map"></div>
-<script>
-    mapboxgl.accessToken = '{{ env('MAPBOX_API_KEY') }}';
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/light-v11', // style URL
-        center: [11.5, 6.5], // Centre géographique du Cameroun
-        zoom: 6 // Zoom initial
-    });
+<x-map-master>
+    <div id="map"></div>
+    <script>
+        mapboxgl.accessToken = "pk.eyJ1IjoiZGlsYW5lMDUiLCJhIjoiY2xreWJydjNxMGd5aDNtc2lsMG5uYnU5ayJ9.WBERCXWXNAEzQWfwc1RwlA";
+        const map = new mapboxgl.Map({
+            container: 'map', // container ID
+            style: 'mapbox://styles/mapbox/light-v11', // style URL
+            center: [11.5, 6.5], // Centre géographique du Cameroun
+            zoom: 6 // Zoom initial
+        });
 
-    // Coordonnées des limites géographiques du Cameroun
-    var coordinates = [
-        [8.48833621698, 2.11704563528], // Coin sud-ouest
-        [16.0128524106, 13.0782712405]  // Coin nord-est
-    ];
+        // Coordonnées des limites géographiques du Cameroun
+        var coordinates = [
+            [8.48833621698, 2.11704563528], // Coin sud-ouest
+            [16.0128524106, 13.0782712405] // Coin nord-est
+        ];
 
-    map.fitBounds(coordinates);
+        map.fitBounds(coordinates);
 
-    map.on('load', () => {
-        // Données des titres fonciers au format GeoJSON
-        var geojson = {
-            'type': 'FeatureCollection',
-            'features': [
-                @foreach ($titles as $title) 
-                {
+        map.on('load', () => {
+            // Add a data source containing GeoJSON data.
+            map.addSource('maine', {
+                'type': 'geojson',
+                'data': {
                     'type': 'Feature',
                     'geometry': {
                         'type': 'Polygon',
+                        // These coordinates outline Maine.
                         'coordinates': [
                             [
-                                {!! json_encode($title->coordonnees) !!}
+                                [564321.00, 452564.00],
+                                [564335.746, 452548.271],
+                                [564315.224, 452531.059],
+                                [564303.601, 452544.471],
+                                [564321.00, 452564.00],
                             ]
                         ]
-                    },
-                    'properties': {
-                        'title': '{{ $title->numero_titre_foncier }}'
                     }
-                },
-                @endforeach
-            ]
-        };
+                }
+            });
 
-        // Ajouter une source de données GeoJSON.
-        map.addSource('titles', {
-            'type': 'geojson',
-            'data': geojson
+            // Add a new layer to visualize the polygon.
+            map.addLayer({
+                'id': 'maine',
+                'type': 'fill',
+                'source': 'maine', // reference the data source
+                'layout': {},
+                'paint': {
+                    'fill-color': '#0080ff', // blue color fill
+                    'fill-opacity': 0.5
+                }
+            });
+            // Add a black outline around the polygon.
+            map.addLayer({
+                'id': 'outline',
+                'type': 'line',
+                'source': 'maine',
+                'layout': {},
+                'paint': {
+                    'line-color': '#000',
+                    'line-width': 3
+                }
+            });
         });
-
-        // Ajouter une couche pour afficher les formes des titres fonciers.
-        map.addLayer({
-            'id': 'titles-layer',
-            'type': 'fill',
-            'source': 'titles',
-            'layout': {},
-            'paint': {
-                'fill-color': '#00FF00', // Couleur verte
-                'fill-opacity': 0.5
-            }
-        });
-    });
-</script>
-</body>
-</html>
+    </script>
+</x-map-master>
