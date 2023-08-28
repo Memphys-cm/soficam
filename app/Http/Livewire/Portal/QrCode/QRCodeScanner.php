0d6f2abf981651538bb62fe0c2a2550f1f72e633
GamaSpeed <franklingameni@gmail.com>
2023-08-28 16:49:20 +0100

    scanner

---
+++ app/Http/Livewire/Portal/QrCode/QRCodeScanner.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Portal\QrCode;
+
+use Livewire\Component;
+
+class QRCodeScanner extends Component
+{
+    public function render()
+    {
+        return view('livewire.portal.qr-code.q-r-code-scanner');
+    }
+}
