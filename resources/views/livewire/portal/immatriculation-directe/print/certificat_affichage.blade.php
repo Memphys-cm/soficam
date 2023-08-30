a2f4e7e06e32f3a39a88bd4524172c4ee3ed53a0
GamaSpeed <franklingameni@gmail.com>
2023-08-30 16:39:16 +0100

    scanner

---
+++ resources/views/livewire/portal/immatriculation-directe/print/certificat_affichage.blade.php
@@ -0,0 +1,82 @@
+<div>
+    <table style="margin-left:0px; margin-right:0px; text-align:center; margin-bottom:2px">
+        <tr>
+            <td>
+                <div>REPUBLIQUE DU CAMEROUN</div>
+                <div>Paix - Travail - Patrie</div>
+                <div>--------------</div>
+            </td>
+            <td style="width: 150px"></td>
+            <td>
+                <div>REPUBLIC OF CAMEROON</div>
+                <div>Peace - Work - Fatherland</div>
+                <div>--------------</div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div>REGION DU ____________</div>
+                <div>--------------</div>
+            </td>
+            <td style="width: 150px"></td>
+            <td>
+                <div>_____________ REGION</div>
+                <div>--------------</div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div>DEPARTEMENT DU ____________</div>
+                <div>--------------</div>
+            </td>
+            <td style="width: 150px"></td>
+            <td>
+                <div>___________ DIVISION</div>
+                <div>--------------</div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div>ARRONDISSEMENT DU _____________</div>
+                <div>--------------</div>
+            </td>
+            <td style="width: 150px"></td>
+            <td>
+                <div>_________ SUBDIVISION</div>
+                <div>--------------</div>
+            </td>
+        </tr>
+    </table>
+    <div style="padding: 20px">
+        <div style="margin-bottom:50px; text-align: center; margin-top:50px; font-size:32px; text-transform:uppercase">
+            <b>certificat d'affichage</b>
+        </div>
+        <div style="text-align: justify; font-size:18px">Le Sous préfet de YAOUNDÉ III Soussigné, certifie avoir apposé du _________ au
+            ____________, l'extrait de la demande de Titre Foncier par M. ________________________ sur une parcelle du
+            Domaine National située dans l'Arrondissement de _____________, au lieu dit __________________.</div>
+        <div style="text-align: justify; margin-top:30px; font-size:18px">En foi de quoi le présent certificat d'affichage est dressé
+            conformément à l'Article 13 du Décrèt 76/165 du 27 Avril 1976 modifié et complété par le Décrèt N° 2005/481
+            du 16 décembre 2005 fixant les conditions du Titre Foncier.</div>
+        <div style="margin-top:50px">
+            <table style="font-size: 14px; padding-left: 20px">
+                <tr>
+                    <td style="text-align: start">
+                        <div>APMLIATIONS : </div>
+                        <div>
+                            <ul>
+                                <li style="margin-left:5px">Préfet du _____(ACTR)</li>
+                                <li style="margin-left:10px">Maire (Pr Info)</li>
+                                <li style="margin-left:15px">Chef du Village (Pr Affichage)</li>
+                            </ul>
+                        </div>
+                    </td>
+                    <td style="width: 150px"></td>
+                    <td style="text-align: end">
+                        <div><b>Yaoundé, le____________</b></div>
+                        <div>Le sous préfèt de l'arrondissement de _________</div>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
