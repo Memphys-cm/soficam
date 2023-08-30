a4a54c49be3a6068871ab5d28e91bdd215eb410e
GamaSpeed <franklingameni@gmail.com>
2023-08-30 22:32:04 +0100

    init

--- resources/views/livewire/portal/immatriculation-directe/print/certificat_affichage.blade.php
+++ resources/views/livewire/portal/immatriculation-directe/print/certificat_affichage.blade.php
@@ -15,34 +15,34 @@
         </tr>
         <tr>
             <td>
-                <div>REGION DU ____________</div>
+                <div>REGION DU {{$certif_affi->region->region_name_fr}}</div>
                 <div>--------------</div>
             </td>
             <td style="width: 150px"></td>
             <td>
-                <div>_____________ REGION</div>
+                <div>{{$certif_affi->region->region_name_en}} REGION</div>
                 <div>--------------</div>
             </td>
         </tr>
         <tr>
             <td>
-                <div>DEPARTEMENT DU ____________</div>
+                <div>DEPARTEMENT DU {{$certif_affi->division->division_name}}</div>
                 <div>--------------</div>
             </td>
             <td style="width: 150px"></td>
             <td>
-                <div>___________ DIVISION</div>
+                <div>{{$certif_affi->division->division_name}} DIVISION</div>
                 <div>--------------</div>
             </td>
         </tr>
         <tr>
             <td>
-                <div>ARRONDISSEMENT DU _____________</div>
+                <div>ARRONDISSEMENT DU {{$certif_affi->subDivision->sub_division_name}}</div>
                 <div>--------------</div>
             </td>
             <td style="width: 150px"></td>
             <td>
-                <div>_________ SUBDIVISION</div>
+                <div>{{$certif_affi->subDivision->sub_division_name}} SUBDIVISION</div>
                 <div>--------------</div>
             </td>
         </tr>
@@ -52,8 +52,10 @@
             <b>certificat d'affichage</b>
         </div>
         <div style="text-align: justify; font-size:18px">Le Sous préfet de YAOUNDÉ III Soussigné, certifie avoir apposé du _________ au
-            ____________, l'extrait de la demande de Titre Foncier par M. ________________________ sur une parcelle du
-            Domaine National située dans l'Arrondissement de _____________, au lieu dit __________________.</div>
+            ____________, l'extrait de la demande de Titre Foncier par M. @foreach ($certif_affi->users as $user)
+                {{$user->first_name}} {{$user->last_name}};
+            @endforeach sur une parcelle du
+            Domaine National située dans l'Arrondissement de {{$certif_affi->subDivision->sub_division_name}}, au lieu dit {{$certif_affi->localisation}}.</div>
         <div style="text-align: justify; margin-top:30px; font-size:18px">En foi de quoi le présent certificat d'affichage est dressé
             conformément à l'Article 13 du Décrèt 76/165 du 27 Avril 1976 modifié et complété par le Décrèt N° 2005/481
             du 16 décembre 2005 fixant les conditions du Titre Foncier.</div>
@@ -64,7 +66,7 @@
                         <div>APMLIATIONS : </div>
                         <div>
                             <ul>
-                                <li style="margin-left:5px">Préfet du _____(ACTR)</li>
+                                <li style="margin-left:5px">Préfet du {{$certif_affi->division->division_name}}(ACTR)</li>
                                 <li style="margin-left:10px">Maire (Pr Info)</li>
                                 <li style="margin-left:15px">Chef du Village (Pr Affichage)</li>
                             </ul>
@@ -73,7 +75,7 @@
                     <td style="width: 150px"></td>
                     <td style="text-align: end">
                         <div><b>Yaoundé, le____________</b></div>
-                        <div>Le sous préfèt de l'arrondissement de _________</div>
+                        <div>Le sous préfèt de l'arrondissement de {{$certif_affi->subDivision->sub_division_name}}</div>
                     </td>
                 </tr>
             </table>
---
+++ resources/views/livewire/portal/immatriculation-directe/print/quitance.blade.php
@@ -0,0 +1,51 @@
+<div class="container">
+    <div style="padding: 7px">
+        <div>Pour servir au paiement de </div>
+        <div style="margin-bottom: 20px; margin-top:10px">Pour le compte de ___________</div>
+        <table border=3 style="margin-top: 10px">
+            <thead style="text-transform: uppercase; text-align:center">
+                <th>quantité</th>
+                <th>désignation</th>
+                <th>prix de l'unité</th>
+                <th>prix total</th>
+            </thead>
+            <tbody style="text-align: justify; font-weight:bold; height:250px">
+                <tr>
+                    <td style="width: 95px"></td>
+                    <td style="width: 375px">
+                        <div style="padding-top:10px">
+                            <div style="text-decoration: underline; ">TRAVAUX PLANIMÉTRIQUES ET CADASTRAUX</div>
+                            <div style="margin-top: 10px">Arrondissement de ________</div>
+                            <div>Lieu dit ________</div>
+                            <div>État du terrain _________</div>
+                            <div>Superficie _________</div>
+                            <div>Forfait pour la recette des domaines________</div>
+                            <div>Majoration 10% pour le cadastre _________</div>
+                            <div>Total __________</div>
+                        </div>
+                        <p></p>
+                        <p></p>
+                        <p></p>
+                        <div style="margin-top: 80px">ARRETE le Présent à la somme de _________</div>
+                    </td>
+                    <td style="width: 95px"></td>
+                    <td style="width: 95px"></td>
+                </tr>
+            </tbody>
+        </table>
+        <div style="margin-top: 20px"><b>Vu et reconnu exact :</b></div>
+        <table>
+            <tr>
+                <td>_______le ______2____</td>
+                <td style="width: 330px"></td>
+                <td>_______le ______2____</td>
+            </tr>
+            <tr style="text-align: center">
+                <td><b>Le Cessionnaire</b></td>
+                <td style="width: 330px"></td>
+                <td><b>Chef service du Cadastre</b></td>
+            </tr>
+        </table>
+    </div>
+
+</div>
