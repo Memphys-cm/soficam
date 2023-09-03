fb27fcca10888f51bcd9f1327384cd0039d113be
Fotsingboris <fotsingtchoupe1@gmail.com>
2023-09-03 11:47:47 +0100

    merged

--- resources/views/livewire/portal/services/index.blade.php
+++ resources/views/livewire/portal/services/index.blade.php
@@ -140,7 +140,7 @@
                         @canany('service.update','service.delete')
                         <td>
                             @can('service.update')
-                            <a href="#" wire:click.prevent="initData({{$service->id}})" data-bs-toggle="modal" data-bs-target="#CreateUpdateServiceModal" draggable="false">
+                            <a href="#" wire:click.prevent="initData({{$service->id}})" data-bs-toggle="modal" data-bs-target="#UpdateServiceModal" draggable="false">
                                 <svg class="icon icon-sm text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
                                 </svg>
