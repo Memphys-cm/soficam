ca783932dcc9d1223fd3c726d4fbafba98dd9b97
Escarter <mbutuhescarter@gmail.com>
2023-09-04 19:57:35 +0100

    fixes

--- resources/views/livewire/portal/certificate-propriete/print.blade.php
+++ resources/views/livewire/portal/certificate-propriete/print.blade.php
@@ -1,7 +1,7 @@
 @php
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
-QrCode::format('png')->size(100)->generate(url("validate-document?model={$element->uuid}&category=certificate_propriete"));
+$qrCode = QrCode::format('png')->size(100)->generate(url("validate-document?model={$element->uuid}&category=certificate_propriete"));
 
 @endphp
 <div class="container-fluid">
@@ -9,19 +9,20 @@ QrCode::format('png')->size(100)->generate(url("validate-document?model={$elemen
         <tr style="font-size: 12px">
             <td>
                 <div style="text-align: center"><b>
-                    <div>REPUBLIQUE DU CAMEROUN</div>
-                    <div>PAIX-TRAVAIL-PATRIE</div>
-                    <div>--------</div>
-                    <div>MINISTERE DES DOMAINES DU CADASTRE ET</div> 
-                    <div>DES AFFAIRES FONCIERES</div>
-                    <div>--------</div>
-                    <div>SECRETARIAT GENERAL</div>
-                    <div>--------</div>
-                    <div>DIVISION DES ETUDES, DE LA PLANIFICATION</div> 
-                    <div>ET DE LA COOPERATION</div>
-                    <div>--------</div>
-                    <div>CELLULE DES ETUDES ET DE LA PLANIFICATION</div>
-                    <div>--------</div></b>
+                        <div>REPUBLIQUE DU CAMEROUN</div>
+                        <div>PAIX-TRAVAIL-PATRIE</div>
+                        <div>--------</div>
+                        <div>MINISTERE DES DOMAINES DU CADASTRE ET</div>
+                        <div>DES AFFAIRES FONCIERES</div>
+                        <div>--------</div>
+                        <div>SECRETARIAT GENERAL</div>
+                        <div>--------</div>
+                        <div>DIVISION DES ETUDES, DE LA PLANIFICATION</div>
+                        <div>ET DE LA COOPERATION</div>
+                        <div>--------</div>
+                        <div>CELLULE DES ETUDES ET DE LA PLANIFICATION</div>
+                        <div>--------</div>
+                    </b>
                 </div>
             </td>
             <td style="width: 4cm; text-align: center;">
@@ -29,19 +30,20 @@ QrCode::format('png')->size(100)->generate(url("validate-document?model={$elemen
             </td>
             <td>
                 <div style="text-align: center"><b>
-                    <div>REPUBLIC OF CAMEROON</div>
-                    <div>PEACE-WORK-FATHERLAND</div>
-                    <div>--------</div>
-                    <div>MINISTRY OF STATE PROPERTY, SURVEYS</div> 
-                    <div>AND LAND TENURE</div>
-                    <div>--------</div>
-                    <div>SECRETARIAT GENERAL</div>
-                    <div>--------</div>
-                    <div>DEPARTMENT OF STUDIES, PLANNING</div> 
-                    <div>AND COOPERATION</div>
-                    <div>--------</div>
-                    <div>UNIT OF STUDIES AND PLANNING</div>
-                    <div>--------</div></b>
+                        <div>REPUBLIC OF CAMEROON</div>
+                        <div>PEACE-WORK-FATHERLAND</div>
+                        <div>--------</div>
+                        <div>MINISTRY OF STATE PROPERTY, SURVEYS</div>
+                        <div>AND LAND TENURE</div>
+                        <div>--------</div>
+                        <div>SECRETARIAT GENERAL</div>
+                        <div>--------</div>
+                        <div>DEPARTMENT OF STUDIES, PLANNING</div>
+                        <div>AND COOPERATION</div>
+                        <div>--------</div>
+                        <div>UNIT OF STUDIES AND PLANNING</div>
+                        <div>--------</div>
+                    </b>
                 </div>
             </td>
         </tr>
@@ -49,7 +51,7 @@ QrCode::format('png')->size(100)->generate(url("validate-document?model={$elemen
 
     <div style="padding: 12px; text-align:center; margin-top:10px">
         <img src="{{'data:image/png;base64,'. base64_encode($qrCode) }} " alt="QR Code">
-        
+
         <!-- <img src="$qrCode" alt="QR Code"> -->
         <!-- <img src="data:image/png;base64, {!! base64_encode($qrCode) !!} "> -->
     </div>
