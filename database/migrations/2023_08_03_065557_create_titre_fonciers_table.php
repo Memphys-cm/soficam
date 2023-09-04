e3c17c3858438bfb2a7d5eee3276a4c09d52e7f2
Dilane <angedilane05@gmail.com>
2023-09-04 22:59:31 +0100

    Working Dashboard 99

--- database/migrations/2023_08_03_065557_create_titre_fonciers_table.php
+++ database/migrations/2023_08_03_065557_create_titre_fonciers_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
            $table->uuid('uuid')->unique()->index();
             $table->string('numero_titre_foncier')->index();
+            $table->string('numero_conservation')->index();
             $table->date('date_de_delivrance_du_TF');
             $table->string('numero_du_duplicata')->nullable();
             $table->foreignId('region_id')->index();
