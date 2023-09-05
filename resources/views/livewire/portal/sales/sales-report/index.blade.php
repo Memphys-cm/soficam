e900fe17a133c4300fb1e83fad5fd61fb65ede39
Escarter <mbutuhescarter@gmail.com>
2023-09-05 11:27:17 +0100

    fixes report parts

--- resources/views/livewire/portal/sales/sales-report/index.blade.php
+++ resources/views/livewire/portal/sales/sales-report/index.blade.php
@@ -8,10 +8,8 @@
                     <ol class="breadcrumb breadcrumb-dark breadcrumb-transparent">
                         <li class="breadcrumb-item">
                             <a href="#">
-                                <svg class="icon icon-xxs" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
+                                <svg class="icon icon-xxs" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
                                     </path>
                                 </svg>
                             </a>
@@ -21,10 +19,8 @@
                     </ol>
                 </nav>
                 <h1 class="h4 mt-n2 d-flex justify-content-start align-items-end">
-                    <svg class="icon me-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z">
+                    <svg class="icon me-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z">
                         </path>
                     </svg>
                     {{ __('Rapport sur toutes les ventes') }}
@@ -37,10 +33,8 @@
                 {{-- @can('user.export_n_print') --}}
                 <div class="mx-2" wire:loading.remove>
                     <a wire:click="export()" class="btn btn-sm btn-gray-500  py-2 d-inline-flex align-items-center ">
-                        <svg class="icon icon-xs me-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"></path>
+                        <svg class="icon icon-xs me-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"></path>
                         </svg>
                         {{ __('Exporter') }}
                     </a>
@@ -67,8 +61,7 @@
             <div class="row">
                 <div class="col-md-3">
                     <label for="search">{{ __('Recherche') }}: </label>
-                    <input wire:model="query" id="search" type="text" placeholder="{{ __('Recherche...') }}"
-                        class="form-control">
+                    <input wire:model="query" id="search" type="text" placeholder="{{ __('Recherche...') }}" class="form-control">
                     {{-- <p class="badge badge-info" wire:model="resultCount">{{$resultCount}}</p> --}}
                 </div>
 
@@ -101,32 +94,29 @@
                         <option value="mtn_mobile_money">{{ __('MTN Mobile Money') }}</option>
                         <option value="orange_money">{{ __('Orange Money') }}</option>
                         <option value="cash">{{ __('Cash') }}</option>
-                        
+
                     </select>
                 </div>
                 <div class="col-md-3 my-2">
                     <label for="startdate">{{ __('Période de démarrage') }}</label>
-                    <input wire:model="startdate" name="startdate" type="date"
-                        class="form-control  @error('inter_start') is-invalid @enderror" required="">
+                    <input wire:model="startdate" name="startdate" type="date" class="form-control  @error('inter_start') is-invalid @enderror" required="">
                 </div>
                 <div class='col-md-3 my-2'>
                     <label for="enddate">{{ __('Date de fin') }}</label>
-                    <input wire:model="enddate" name="enddate" type="date"
-                        class="form-control @error('enddate') is-invalid @enderror" required="">
+                    <input wire:model="enddate" name="enddate" type="date" class="form-control @error('enddate') is-invalid @enderror" required="">
                 </div>
                 <div class="col-md-3">
                     <label for="service">{{ __('Service') }}</label>
-                    <x-input.select wire:model="service_id" prettyname="service_id" :options="$services->pluck('service_name_fr', 'id')->toArray()"
-                        selected="('service')" />
+                    <x-input.select wire:model="service_id" prettyname="service_id" :options="$services->pluck('service_name_fr', 'id')->toArray()" selected="('service')" />
                     @error('service')
-                        <div class="invalid-feedback">{{ $message }}</div>
+                    <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class='col'>
                     <label class="px-2" for="receveur">{{ __('Receveur') }}</label>
                     <x-input.select wire:model="receveur_id" prettyname="receveur_id" :options="$receveurs->pluck('first_name', 'id')->toArray()" />
                     @error('user_ids')
-                        <div class="invalid-feedback">{{ $message }}</div>
+                    <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
 
@@ -165,7 +155,7 @@
                 <label for="notary">{{ __('NOTAIRE') }}</label>
                 <x-input.select wire:model="notary" prettyname="notary" :options="$notaries->pluck('first_name', 'id')->toArray()" selected="('notary')" />
                 @error('notary')
-                    <div class="invalid-feedback">{{ $message }}</div>
+                <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
         </div>
@@ -187,27 +177,26 @@
                 </thead>
                 <tbody>
                     @forelse ($allsales as $allsale)
-                        <tr>
-                            <td><span class="fw-normal">{{ $allsale->sales_code }}</span></td>
-                            <td><span class="fw-normal">{{ $allsale->user->name }}</span></td>
-                            <td><span class="fw-normal">{{ $allsale->sales_type }}</span></td>
-                            <td><span class="fw-normal">{{ $allsale->sales_amount }} {{ __('FCFA') }} </span></td>
-                            <td><span class="fw-normal">{{ $allsale->payment_method }}</span></td>
-                            <td>
-                                <span
-                                    class="fw-normal badge super-badge p-2 bg-{{ $allsale->statusStyle }} round">{{ $allsale->payment_status }}</span>
+                    <tr>
+                        <td><span class="fw-normal">{{ $allsale->sales_code }}</span></td>
+                        <td><span class="fw-normal">{{ !empty($allsale->user) ? $allsale->user->name : '' }}</span></td>
+                        <td><span class="fw-normal">{{ $allsale->sales_type }}</span></td>
+                        <td><span class="fw-normal">{{ $allsale->sales_amount }} {{ __('FCFA') }} </span></td>
+                        <td><span class="fw-normal">{{ $allsale->payment_method }}</span></td>
+                        <td>
+                            <span class="fw-normal badge super-badge p-2 bg-{{ $allsale->statusStyle }} round">{{ $allsale->payment_status }}</span>
 
-                            </td>
-                            <td><span class="fw-normal">{{ $allsale->created_at->format('Y-m-d') }}</span></td>
-                        </tr>
+                        </td>
+                        <td><span class="fw-normal">{{ $allsale->created_at->format('Y-m-d') }}</span></td>
+                    </tr>
                     @empty
-                        <tr>
-                            <td colspan="9" class="text-center">
-                                <div class="text-center text-gray-800 mt-2">
-                                    <h4 class="fs-4 fw-bold">{{ __('Liste Vide') }} </h4>
-                                </div>
-                            </td>
-                        </tr>
+                    <tr>
+                        <td colspan="9" class="text-center">
+                            <div class="text-center text-gray-800 mt-2">
+                                <h4 class="fs-4 fw-bold">{{ __('Liste Vide') }} </h4>
+                            </div>
+                        </td>
+                    </tr>
                     @endforelse
 
 
